b3fde2d6cc2b5bac0fa215546045d153a9bac49a
brytran <98930713+brytran@users.noreply.github.com>
2022-11-27 22:43:37 -0500

    Add files via upload
    Final database changes

--- toxic_database.py
+++ toxic_database.py
@@ -16,8 +16,9 @@ def create_server_connection(host_name, user_name, user_password):
 
     return connection
 
-def create_database(connection, query):
+def create_database(connection):
     cursor = connection.cursor()
+    query = "CREATE DATABASE Discord"
     try:
         cursor.execute(query)
         connection.commit()
@@ -61,26 +62,53 @@ def read_query(connection, query):
     except Error as err:
         print(f"Error: '{err}'")
 
-def create_db(name, connection):
-    query = "CREATE DATABASE {}".format(name)
-    execute_query(connection, query)
 
 def create_table(db_connection):
-    query = "CREATE TABLE offenses (user_id INT PRIMARY KEY, number_of_offenses INT);" 
-    create_database(db_connection, query)
+    try:
+        query = "CREATE TABLE IF NOT EXISTS offenses (user_id BIGINT, number_of_offenses INT);"
+        execute_query(db_connection, query)
+        print("Creation Successful.")
+    except:
+        print("Creation Failed.")
 
-def insert_new_user(user_id, db_connection):
-    query = "INSERT INTO offenses(user_id, number_of_offenses)" \
-                    "VALUES({}, 1)".format(user_id)
+def insert_new_user(usr, db_connection):
+    query = "INSERT INTO offenses (user_id, number_of_offenses) VALUES ({}, '{}')".format(usr.id, 1)
     execute_query(db_connection, query)
 
-def update_offense_count(user_id, db_connection):
+def update_offense_count(usr, db_connection, ofs):
     update = """
              UPDATE offenses 
-             SET number_of_offenses = number_of_offenses + 1 
-             WHERE user_id = {};
-             """.format(user_id)
+             SET number_of_offenses = {} 
+             WHERE user_id = {}
+             """.format(ofs+1, usr.id)
     execute_query(db_connection, update)
+
+def insert_server_setting(db_connection, sense, limit, alert_channel, server_id):
+    query = "INSERT INTO settings (server, offense_limit, sensitivity, alert_channel) VALUES ({}, {}, {}, {})".format(server_id, limit, sense, alert_channel)
+    execute_query(db_connection, query)
+
+def update_settings(db_connection, sense, limit, alert_channel, server_id):
+    update = """
+             UPDATE settings 
+             SET offense_limit = {}
+             WHERE server = {}
+             """.format(limit, server_id)
+    execute_query(db_connection, update)
+
+    update = """
+            UPDATE settings
+            SET sensitivity = {}
+            WHERE server = {}
+            """.format(sense, server_id)
+    execute_query(db_connection, update)
+
+    update = """
+            UPDATE settings
+            SET alert_channel = {}
+            WHERE server = {}
+            """.format(alert_channel, server_id)
+    execute_query(db_connection, update)
+
 
 def fetch_user_data(user_id, db_connection):
     fetch = """
@@ -97,5 +125,24 @@ def fetch_all_data(db_connection):
             FROM offenses;
             """
     return read_query(db_connection, fetch)
+
+def fetch_all_setttings_data(db_connection):
+    fetch = """
+            SELECT *
+            FROM settings;
+            """
+    return read_query(db_connection, fetch)
+
+def get_offenses(db_connection, usr):
+    fetch = """
+            SELECT number_of_offenses 
+            FROM offenses 
+            WHERE user_id = {}
+            """.format(usr.id)
+    return read_query(db_connection, fetch)
+
+def create_setting_table(db_connection):
+    query = "CREATE TABLE IF NOT EXISTS settings (server BIGINT, offense_limit INT, sensitivity FLOAT, alert_channel BIGINT);"
+    execute_query(db_connection, query)
 
 
--- toxic_detector.py
+++ toxic_detector.py
@@ -1,36 +1,47 @@
-from toxic_database import *
 import discord
+from toxic_database import *
 from discord.ui import Button, View
 from googleapiclient import discovery
+from dotenv import load_dotenv
 import json
 import os
 
-API_KEY = ""
-token = ""
-cid = 1030517807336673320
-mid = 1041513715343818752
-login_info = ["localhost", "root", "password"] #in the future, I think we can implement some kind of login
-db_name = "data"                               # via the database with the settings
-creation = False
+# Getting all secrets
 
+load_dotenv()
+
+API_KEY = "AIzaSyDbyDAhqvWokHXQrDk024syXwFA9wzGuCQ"
+token = "MTAzMDUxMTgwNDc0MjM4MTY2OA.Gumscr.hC1yolMPFfuBkvRq5BiXpn536_-WLNiAe77euw"
+
+#User will need to alter "login_info" to their own desired info
+login_info = ["localhost", "root", "0210"] 
+db_name = "Discord"
+
+
+#Initializing database and establishing connection
+try:
+  connection = create_db_connection(login_info[0], login_info[1], login_info[2], db_name)
+  current_settings = fetch_all_setttings_data(connection)
+except:
+  connection = create_server_connection(login_info[0], login_info[1], login_info[2])
+  create_database(connection)
+  connection = create_db_connection(login_info[0], login_info[1], login_info[2], db_name)
+  create_setting_table(connection)
+  create_table(connection)
+  current_settings = fetch_all_setttings_data(connection)
+
+
+# Giving the bot Intents
 
 intents = discord.Intents()
 intents.message_content = True
 intents.messages = True
 intents.guilds = True
 
-if creation == False:
-  try:
-    connection = create_server_connection(login_info[0], login_info[1], login_info[2])
-    create_db(db_name, connection)
-    connection = create_db_connection(login_info[0], login_info[1], login_info[2], db_name)
-    create_table(connection)
-    creation = True
-  except:
-    creation = False
-
-
 client = discord.Bot(command_prefix="+", intents=intents)
+mod_channel = 1041513715343818752
+
+# Initializing Perspective API
 
 ai_client = discovery.build(
   "commentanalyzer",
@@ -40,73 +51,156 @@ ai_client = discovery.build(
   static_discovery=False,
 )
 
-
 @client.event
 async def on_ready():
     print("We have logged in as {0.user}".format(client))
 
+# Succesful Run Message
+
+if current_settings == []:
+  @client.event
+  async def on_guild_join(guild):
+    for channel in guild.text_channels:
+        if channel.permissions_for(guild.me).send_messages:
+            await channel.send("""Hello! Thanks for inviting me! 🙏
+                                \nTo function properly, I need admins to update my settings! 
+                                \nYou can do this by using '/settings'.""")
+        break
+
 @client.event
 async def on_message(message):
-
-    button = Button(label="Delete Message", style=discord.ButtonStyle.green, emoji="🗑️")
-    button2 = Button(label="Message Deleted", style=discord.ButtonStyle.green, emoji="✅", disabled=True)
-    view = View()
-    view.add_item(button)
-
-
-    if message.author == client.user:
-        return
-    usr = message.author
-    msg = message.content
-    gld = message.guild
-    print(msg)
-    channel1 = client.get_channel(cid)
-    channel2 = message.channel
-    analyze_request = {
-    'comment': { 'text': msg },
-    'requestedAttributes': {'SEVERE_TOXICITY': {}}
-    }
-    response = ai_client.comments().analyze(body=analyze_request).execute()
-    print(response)
-    value = response['attributeScores']['SEVERE_TOXICITY']['summaryScore']['value']
-
-    embed2 = discord.Embed(
-            title=f'{usr.name} just sent a potentially harmful message',
-            description=f"User ID: `{usr.id}`")
-    embed2.add_field(name=f"Contents:", value=f"```{msg}```")
-    embed2.add_field(name=f"Score:", value=f"```{value}```")
-    embed2.add_field(name=f"Channel:", value=f"{channel2.mention}")
-    embed2.add_field(name=f"Message Link:", value=f"https://discord.com/channels/{gld.id}/{channel2.id}/{message.id}")
+    if int(message.channel.id) != mod_channel and current_settings != []:
+      button = Button(label="Delete Message", style=discord.ButtonStyle.green, emoji="🗑️")
+      button2 = Button(label="Message Deleted", style=discord.ButtonStyle.green, emoji="✅", disabled=True)
+      view = View()
+      view.add_item(button)
 
 
-    async def button_callback(interaction):
-      await message.delete()
-      view.clear_items()
-      view.add_item(button2)
-      await interaction.response.edit_message(view = view, embed=embed2)
-    button.callback = button_callback
-    if value > 0.65:  #this is where I made a lot of my changes
-        user_data = fetch_user_data(usr, connection)
-        mod_channel = client.get(mid)
-        if user_data == []:
-          insert_new_user(usr, connection)
-        else:
-          update_offense_count(usr, connection)
-        
-        user_data = fetch_all_data(usr, connection)
-        message_to_mod = """Warning! User {} has commited an offense in a text channel! 
-                        \nThey have said: {}. 
-                        \nThey have also commited {} offenses. 
-                        \nI recommend review over this offense.""".format(usr, msg, user_data[1])
-        await message.add_reaction('🚩')
-        await channel1.send(embed=embed2, view=view)
-        await mod_channel.send(message_to_mod)
+      button3 = Button(label="Ban", style=discord.ButtonStyle.red, emoji="🔨")
+      view2 = View()
+      view2.add_item(button3)
 
-    
+      if message.author == client.user:
+          return
+
+      usr = message.author
+      msg = message.content
+      gld = message.guild
+
+      print(msg)
+      channel1 = client.get_channel(current_settings[0][3])
+      channel2 = message.channel
+      
+
+      analyze_request = {
+      'comment': { 'text': msg },
+      'requestedAttributes': {'SEVERE_TOXICITY': {}}
+      }
+
+      response = ai_client.comments().analyze(body=analyze_request).execute()
+      print(response)
+      value = response['attributeScores']['SEVERE_TOXICITY']['summaryScore']['value']
+
+      embed2 = discord.Embed(
+              title=f'{usr.name} just sent a potentially harmful message',
+              description=f"User ID: `{usr.id}`")
+      embed2.add_field(name=f"Contents:", value=f"```{msg}```")
+      embed2.add_field(name=f"Score:", value=f"```{value}```")
+      embed2.add_field(name=f"Channel:", value=f"{channel2.mention}")
+      embed2.add_field(name=f"Message Link:", value=f"https://discord.com/channels/{gld.id}/{channel2.id}/{message.id}")
+
+      
+
+      async def button_callback(interaction):
+        await message.delete()
+        view.clear_items()
+        view.add_item(button2)
+        await interaction.response.edit_message(view = view, embed=embed2)
+
+      button.callback = button_callback
+
+      #Checking if message is too toxic
+      sensitivity = current_settings[0][1]
+      limit = current_settings[0][2]
+      if value >= sensitivity:  
+          await message.add_reaction('🚩')
+          await channel1.send(embed=embed2, view=view)
+
+          # Gets offenses and checks if it exists
+          values = get_offenses(connection, usr)
+          if value == []: #if it doesn't exist, it will add user
+            insert_new_user(usr, connection)
+          else: #if it does exist, it will update the count
+            ofs = int(values[0][0])
+            update_offense_count(usr, connection, ofs)
+          values = get_offenses(connection, usr)
+          
+          #if user has gone past the limit of offenses, a special message will send to the moderators
+          if values[0][0] > limit: 
+            embed3 = discord.Embed(
+              title=f'{usr.name} has gone past the offence limit of {current_settings[0][2]}',
+              description=f"User ID: `{usr.id}`")
+            embed3.add_field(name=f"Last Message:", value=f"```{msg}```")
+            embed3.add_field(name=f"Score:", value=f"```{value}```")
+            embed3.add_field(name=f"Offenses:", value= values[0][0])
+            embed3.add_field(name=f"Message Link:", value=f"https://discord.com/channels/{gld.id}/{channel2.id}/{message.id}")
+            await channel1.send(embed=embed3, view=view2)
+            
+
 
 @client.slash_command(name="hi")
 async def hey(ctx):
   await ctx.send("Hey!!")
 
+
+@client.slash_command(name = "settings", description = "Use this command to change the bot settings!") 
+#for this to work, be sure to add your bot to the mod channel!
+async def settings(ctx):
+  global current_settings
+  if int(ctx.channel_id) == current_settings[0][3]:
+    #The following will collect the desired system settings from a moderator
+    view = View()
+    embed1 = discord.Embed(title = """Please tell me your desired sensitivity of the bot! 
+                                    \nThe number inputted should be between 0-1.
+                                    \n0 meaning not sensitive to inappropiate messages and 1 being very sensitive.""")
+    embed2 = discord.Embed(title = """Please tell me your desired offense limit!
+                                    \nThe limit determines how many inappropiate messages you will allow users to send before we notify you.""")
+    embed3 = discord.Embed(title = """Please tell me your desired alert channel!
+                                    \nThis channel will be used to send moderators alerts about users.""")
+
+    await ctx.send(embed = embed1, view = view)
+    view.clear_items()
+    msg = await client.wait_for("message")
+    new_sense = float(msg.content)
+
+    await ctx.send(embed = embed2, view = view)
+    view.clear_items()
+    msg = await client.wait_for("message")
+    new_limit = int(msg.content)
+
+    await ctx.send(embed = embed3, view = view)
+    view.clear_items()
+    msg = await client.wait_for("message")
+    new_modchannel = int(msg.content)
+
+    serverid = int(ctx.guild.id)
+
+    #The database will update, given that the types are correct
+    try:
+      current_settings = fetch_all_setttings_data(connection)
+      if new_sense <= 0 or new_sense >1: #checking if the sensitivity makes sense
+        await ctx.send("There was some kind of error 😢")
+      else:
+        if current_settings == []:
+          insert_server_setting(connection, new_sense, new_limit, new_modchannel, serverid)
+        else:
+          update_settings(connection, new_sense, new_limit, new_modchannel, serverid)
+        await ctx.send("Settings Updated!")
+        await ctx.send('😃')
+    except:
+      await ctx.send("There was some kind of error 😢")
+  else: #This will occur if the use of this command didn't occur in the alert channel
+    await ctx.send("Permission Denied.")
+    await ctx.send('👿')
 
 client.run(token)
